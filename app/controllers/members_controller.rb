9bcea23cd2adb502acb3ed3b88bc459d77b8f1a2
jamestucker1988 <xat2212@gmail.com>
2021-03-12 14:29:53 +0530

    members controller

---
+++ app/controllers/members_controller.rb
@@ -0,0 +1,18 @@
+class MembersController < ApplicationController
+  def index
+    @member = Member.all
+  end
+  def new 
+    @member = Member.new
+  end
+  def create 
+    # @member = Member.find(params[:id])
+    # 
+    if @member.save
+      @member = Member.new(params[:id]) if params[:id]
+      redirect_to members_path, notice: "members list"
+    else
+      render  :new
+    end
+  end
+end
